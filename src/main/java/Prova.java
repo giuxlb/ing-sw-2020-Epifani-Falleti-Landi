6c1fb5655a38abfd6f7891fd390111e8601ae236
adrianofalleti98 <10566971@polimi.it>
2020-03-26 20:29:24 +0100

    Pom modified

--- src/main/java/Prova.java
+++ src/main/java/Prova.java
@@ -1,5 +1,8 @@
-public class Prova {
-    public static void main(String[] args){
+public class Prova
+{
+    public static void main(String[] args)
+    {
         System.out.println("HelloWorld");
     }
+
 }
