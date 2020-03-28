02fd6d468bf1298ec259f45cd6b5bd72e630993a
alfredolandi97 <alfredo.landi@mail.polimi.it>
2020-03-28 13:47:38 +0100

    First implementation

--- src/main/java/Model/Board.java
+++ src/main/java/Model/Board.java
@@ -1,5 +1,32 @@
 package Model;
 
 public class Board {
+    private final static int DIM=5;
     private BoardCell[][] boardGame;
+
+    /***
+     * Constructor for this class
+     */
+    public Board(){
+        this.boardGame= new BoardCell[DIM][DIM];
+
+        for(int i=0;i<DIM;i++){
+            for(int j=0;j<DIM;j++){
+                this.boardGame[i][j]= new BoardCell();
+            }
+        }
+    }
+
+    /***
+     * Reset boardGame to initial conditions
+     */
+    public void reset(){
+        for(int i=0;i<DIM;i++){
+            for(int j=0;j<DIM;j++){
+                this.boardGame[i][j].setHeight(0);
+                this.boardGame[i][j].setWorker(null);
+            }
+        }
+    }
+
 }
--- src/main/java/Model/BoardCell.java
+++ src/main/java/Model/BoardCell.java
@@ -3,4 +3,29 @@ package Model;
 public class BoardCell {
     private int height;
     private Worker workerBuilder;
+
+    /***
+     * Put a worker on this BoardCell
+     * @param workerBuilder
+     */
+    public void setWorker(Worker workerBuilder){
+        this.workerBuilder=workerBuilder;
+    }
+
+    /***
+     * Set height of current BoardCell to 0 (0 floor), 1 (1st floor), 2 (2nd floor), 3 (3rd floor) or 4 (dome)
+     * @param height
+     */
+    public void setHeight(int height){
+        this.height=height;
+    }
+
+    /***
+     * Constructor for this class
+     */
+    public BoardCell(){
+        this.height=0;
+        this.workerBuilder=null;
+    }
+
 }
