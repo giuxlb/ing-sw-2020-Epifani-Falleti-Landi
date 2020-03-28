9cc500d59474af86a80031a41352e71fa8f54c85
giuxlb <61745729+giuxlb@users.noreply.github.com>
2020-03-28 13:36:57 +0100

    Added some methods to the Worker and Player classes

--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -2,6 +2,32 @@ package Model;
 
 public class Player {
     private String username;
-    private Worker workers[2];
+    private Worker[] workers;
     private Card gameCard;
+
+    /***
+     * Costruttore della classe Player
+     * @param name nome da assegnare al giocatore
+     */
+    public Player (String name){
+        this.username = name;
+    }
+
+    /***
+     * Setter of the card
+     * @param card the card to assigng to the Player
+     */
+    public void chooseCard(Card card){
+        this.gameCard=card;
+    }
+
+    /***
+     * calls the method to move a worker.
+     * @param worker the worker to move
+     * @param x new position x of the worker
+     * @param y new position y of the worker
+     */
+    public void moveWith(Worker worker,int x, int y){
+        worker.moveTo(x,y);
+    }
 }
--- src/main/java/Model/Worker.java
+++ src/main/java/Model/Worker.java
@@ -1,7 +1,48 @@
 package Model;
 
 public class Worker {
-    private Colors color;
+    private Color color;
     private int pos_x;
     private int pos_y;
+
+    /***
+     * Constructor of the worker class. TO MODIFY:
+     * in this state it can't notify to the Board that a new worker
+     * is added. It also can't check if the position is legal.
+     * @param initial_x the position x on the board
+     * @param initial_y the position y on the board
+     * @param color the color of the worker
+     */
+    public Worker(int initial_x,int initial_y,Color color){
+        this.color=color;
+        this.pos_x=initial_x;
+        this.pos_y=initial_y;
+    }
+
+    /***
+     * Setter of the positions.Doesn't check if the new position is legal.
+     * @param x the new x position
+     * @param y the nex y position
+     */
+    public void moveTo(int x,int y){
+        this.pos_x=x;
+        this.pos_y=y;
+    }
+
+    /***
+     *
+     * @return position X of the Worker
+     */
+    public int getPositionX(){
+        return this.pos_x;
+    }
+
+    /***
+     *
+     * @return position Y of the worker
+     */
+    public int getPositionY(){
+        return this.pos_y;
+    }
+
 }
