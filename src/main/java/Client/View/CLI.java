9570319c1512b527846a991c3bc494debc754dde
Alfredo Landi <alfredo.landi@mail.polimi.it>
2020-04-29 18:27:44 +0200

    ClI 50% completed

--- src/main/java/Client/View/CLI.java
+++ src/main/java/Client/View/CLI.java
@@ -9,6 +9,7 @@ import Controller.Coordinates;
 import Controller.Network.VCEvent;
 import Model.Color;
 import Model.Worker;
+import
 
 
 import java.util.ArrayList;
@@ -48,6 +49,7 @@ public class CLI {
                 System.out.println("Errore, impossibile avviare una partita con questo numero di giocatori, chiedere solo per 2 o 3 giocatori");
                 n = mainScanner.nextInt();
             }
+            CLI.setPlayersNumber(n);
             CLI.buildEvent(cnh, n, VCEvent.Event.setup_request);
 
             //Le pedine del primo player sono gialle
@@ -59,24 +61,6 @@ public class CLI {
             //Le pedine di un EVENTUALE terzo player sono viola
             CLI.setColor(Color.ANSI_PURPLE);
         }
-
-
-        /*System.out.println("Caricamento in corso...");
-        //getBoard() from server
-        currentCLI.printBoard(b);
-        System.out.println("Dove vuoi inserire il primo worker?");
-        //Chi li fa i controlli sulla posizione
-        //Controlla la view se il formato che è corretto e il controller se il client non ha preso il posto di un altro?
-        System.out.println("Selezionare l'i-esima posizione");
-        s.nextInt();
-        System.out.println("Selezionare la j-esima positione");
-        s.nextInt();
-        //Nel mezzo stampo la board?
-        System.out.println("Dove vuoi inserire il secondo worker?");
-        System.out.println("Selezionare l'i-esima posizione");
-        s.nextInt();
-        System.out.println("Selezionare la j-esima positione");
-        s.nextInt();*/
 
 
 
@@ -92,72 +76,103 @@ public class CLI {
     private Board b;
     private String move="dove ti vuoi muovere";
     private String build="dove vuoi costruire";
+    private Elements e;
+    private String xPosition ="y";
+    private String yPosition = "x";
+    private String movingPhase = "Fase di movimento";
+    private String buildingPhase = "Fase di costruzione";
+    private int playersNumber;
 
 
     //Costruttore della CLI
+
+    /***
+     *
+     */
     public CLI(){
-        s= new Scanner(System.in);
-        c=new Controller();
-        b= new Board();
+        s = new Scanner(System.in);
+        c = new Controller();
+        b = new Board();
+        e = new Elements();
     }
 
-    //da aggiustare una volta finita l'unificazione con ClientNetworkHandler
+    /***
+     *
+     * @param b
+     */
     public void printBoard(Board b){
         for(int i=0;i<5;i++){
             for(int j=0;j<5;j++)  {
-                if(b.getBoardWorker(i,j)==null){
-                    System.out.print( Color.ANSI_BLUE +"| worker: X" + " height: " + b.getBoardHeight(i,j) + "| ");
-                }else{
+                    System.out.print( Color.ANSI_BLUE +"| worker: " + turnWorkerIntoImage(b, i, j) + " height: " + turnHeightIntoImage(b, i, j) + " | ");
+                    }
+            System.out.println();
+        }
+    }
 
+    /***
+     *
+     * @param b
+     * @param validPositions
+     */
+    public void paintBoardCell(Board b, ArrayList<Coordinates> validPositions){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                for(Coordinates c: validPositions){
+                    if(c.getX()==i && c.getY()==j){
+                        System.out.print( Color.ANSI_BLUE +"| worker: " + turnWorkerIntoImage(b, i, j) + " height: " + turnHeightIntoImage(b, i, j) + " | ");
+                    }else{
+                        System.out.print( Color.ANSI_BLUE +"| worker: " + turnWorkerIntoImage(b, i, j) + " height: " + turnHeightIntoImage(b, i, j) + " | ");
+                    }
                 }
             }
             System.out.println();
         }
     }
 
-    //da aggiustare una volta finita l'unificazione con ClientNetworkHandler
-    public void paintBoardCell(Board b, ArrayList<Coordinates> validPositions){
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                for(Coordinates c: validPositions){
-                    if(c.getX()==i && c.getY()==j){
-                        System.out.print( Color.ANSI_BLUE +"| worker: X" + " height: " + b.getBoardHeight(i,j) + "| ");
-                    }
-                }
-            }
-        }
-    }
-
-    //da aggiustare una volta finita l'unificazione con ClientNetworkHandler
-    public void printWorker(Board b, int i, int j, Color c){
-        if(b.getBoardWorker(i,j)==null){
-            System.out.print( c +"| worker: X" + " height: " + b.getBoardHeight(i,j) + "| ");
-        }else{
-            System.out.print( Color.ANSI_BLUE +"| worker: " /*+*/  + " height: " + b.getBoardHeight(i,j) + "| ");
-        }
-    }
-
+    /***
+     *
+     * @return
+     */
     public int getPlayerID() {
         return playerID;
     }
 
+    /***
+     *
+     * @param playerID
+     */
     public void setPlayerID(int playerID) {
         this.playerID = playerID;
     }
 
+    /***
+     *
+     * @param cnh
+     * @param o
+     * @param command
+     */
     public void buildEvent(ClientNetworkHandler cnh, Object o, VCEvent.Event command){
         VCEvent currentEvent= new VCEvent(o, command);
         cnh.sendVCEvent(currentEvent);
     }
 
+    /***
+     *
+     * @param cnh
+     */
     public void waitUpdateView(ClientNetworkHandler cnh){
         while(cnh.isUpdateView()==false){
             //lo facciamo addormentare
         }
     }
 
+    /***
+     *
+     * @param cnh
+     */
     public void checkEvent(ClientNetworkHandler cnh){
-        while(true){
+        boolean endGame = false;
+        while(endGame==false){
             waitUpdateView(cnh);
             switch (cnh.getFromServer().getCommand()){
                 case username_request :
@@ -182,23 +197,48 @@ public class CLI {
                         Board b = (Board)objectBoard;
                         printBoard(b);
                         setBoard(b);
-                        //Abbiamo bisogno che update venga sempre mandato prima di send_cells_move (Da chiedere a Peppe)
+                        //Abbiamo bisogno che update venga sempre mandato prima di send_cells_move o send_cells_build (Da chiedere a Peppe)
                     }else {
                         System.out.println("Errore, board corrotta!");
                     }
                     break;
                 case send_cells_move:
-                    Object objectValidPositions = cnh.getFromServer().getBox();
-                    ArrayList<Coordinates> validPositions = (ArrayList<Coordinates>)objectValidPositions;
-                    paintBoardCell(b, validPositions );
-                    //Attenzione qui non possiamo controllare se il dato arrivi corrotto o meno!
-                    System.out.println("Selezionare l'i-esima posizione " + getStringMove());
-                    s.nextInt();
-                    System.out.println("Selezionare la j-esima positione" +get);
-                    s.nextInt();
-                        break;
-
-
+                    sendCells(movingPhase, cnh, VCEvent.Event.send_cells_move);
+                    break;
+                case send_cells_build:
+                    sendCells(buildingPhase, cnh, VCEvent.Event.send_cells_build);
+                    break;
+                case you_lost:
+                    Object objectWinner = cnh.getFromServer().getBox();
+                    if(objectWinner instanceof String){
+                        String winner = (String) objectWinner;
+                        System.out.println("Partita conclusa! Il vincitore è: " + winner);
+                    }else{
+                        System.out.println("Errore! La stringa del vincitore è corrotta");
+                    }
+                    System.out.println();
+                    VCEvent lose= new VCEvent("Ho perso", VCEvent.Event.you_lost);
+                    endGame=true;
+                    break;
+                case you_won:
+                    System.out.println("Congratulazioni, sei il vincitore!");
+                    VCEvent win= new VCEvent("Ho vinto", VCEvent.Event.you_won);
+                    endGame=true;
+                    break;
+                    //Da scommentare quando ci sarà l'evento game_ended_foryou
+                /*case game_ended_foryou:
+                    //Ricorda a Peppe che ci sono due modi per perdere
+                    System.out.println("Mi spiace, non puoi più muoverti con nessuno dei tuoi worker");
+                    break;*/
+                case send_all_cards:
+                    System.out.println("Scegli " + getPlayersNumber() + "carte");
+                    break;
+                case wrongInitialPositionMessage:
+                    break;
+                case send_chosen_cards:
+                    break;
+                case choose_initial_position:
+                    break;
             }
         }
     }
@@ -219,13 +259,145 @@ public class CLI {
         return ansiColor;
     }
 
+    /***
+     *
+     * @return
+     */
     public Board getBoard() {
         return b;
     }
 
+    /***
+     *
+     * @param b
+     */
     public void setBoard(Board b) {
         this.b = b;
     }
 
+    /***
+     *
+     * @return
+     */
+    public String getStringMove() {
+        return move;
+    }
 
+    /***
+     *
+     * @return
+     */
+    public String getStringBuild() {
+        return build;
+    }
+
+    /***
+     *
+     * @param b
+     * @param i
+     * @param j
+     * @return
+     */
+    public String turnHeightIntoImage(Board b, int i, int j){
+        if(b.getBoardHeight(i,j)==0){
+            return "   ";
+        }else if(b.getBoardHeight(i,j)==1){
+            return e.getBuilding(1);
+        }else if(b.getBoardHeight(i,j)==2){
+            return e.getBuilding(1) + " " + e.getBuilding(2);
+        }else if(b.getBoardHeight(i,j)==3){
+            return " " + e.getBuilding(3) + " ";
+        }else if(b.getBoardHeight(i,j)==4){
+            return " " + e.getBuilding(4) + " ";
+        }
+        return "Errore nella costuzione dell'altezza";
+    }
+
+    /***
+     *
+     * @param b
+     * @param i
+     * @param j
+     * @return
+     */
+    public String turnWorkerIntoImage(Board b, int i, int j){
+        if(b.getBoardWorker(i,j)!=null){
+            return " " + e.getWorker() + " ";
+        }else{
+            return "   ";
+        }
+    }
+
+    /***
+     *
+     * @param type
+     * @return
+     */
+    public int chooseCoordinate(String type){
+        System.out.println("Inserisci posizione " + type);
+        int coordinate = s.nextInt();
+        while(c.checkLimits(coordinate)==false){
+            System.out.println("Errore! Puoi scegliere solo valori tra 0 e 4, inserisci nuovamente la posizione " + type);
+            coordinate=s.nextInt();
+        }
+
+        return coordinate;
+    }
+
+    /***
+     *
+     * @param validPositions
+     * @param chosenCoordinates
+     * @return
+     */
+    public Integer findIndex(ArrayList<Coordinates> validPositions, Coordinates chosenCoordinates){
+        int index=0;
+        for(Coordinates c:validPositions){
+            if(c.equals(chosenCoordinates)){
+                return index;
+            }
+            index++;
+        }
+        //Se qualcosa è andato storto il controller lato server riceverà un -1
+        return  -1;
+    }
+
+    /***
+     *
+     * @param phase
+     * @param cnh
+     * @param command
+     */
+    public void sendCells(String phase, ClientNetworkHandler cnh, VCEvent.Event command){
+        System.out.println(phase);
+        Object objectValidPositions = cnh.getFromServer().getBox();
+        ArrayList<Coordinates> validPositions = (ArrayList<Coordinates>)objectValidPositions;
+        //Abbiamo bisogno che update venga sempre mandato prima di send_cells_move o send_cells_build (Da chiedere a Peppe)
+        paintBoardCell(b, validPositions );
+        //Attenzione: qui non possiamo controllare se il dato arrivi corrotto o meno!
+        int x=chooseCoordinate(xPosition);
+        int y=chooseCoordinate(yPosition);
+        Coordinates chosenCoordinates = new Coordinates(x,y);
+        while(c.checkRequestedPosition(validPositions, chosenCoordinates)==false){
+            System.out.println("Errore! Mossa non valida, controlla le caselle in rosso per poter eseguire una mossa valida");
+        }
+        VCEvent replyPosition = new VCEvent(findIndex(validPositions, chosenCoordinates), command);
+        cnh.sendVCEvent(replyPosition);
+    }
+
+    /***
+     *
+     * @return
+     */
+    public int getPlayersNumber() {
+        return playersNumber;
+    }
+
+    /***
+     *
+     * @param playersNumber
+     */
+    public void setPlayersNumber(int playersNumber) {
+        this.playersNumber = playersNumber;
+    }
 }
